a454f052e9abeaa15931dccfb9a1744bac366022
MatthiasChometon <matthiaschometon787@gmail.com>
2022-10-11 17:00:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+### Development
+
+Just run and visit http://localhost:3333
+
+```bash
+pnpm dev
+```
+
+### Build
+
+To build the App, run
+
+```bash
+pnpm build
+```
---
+++ src/common/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <q-layout view="lhr lpr lfr">
+    <q-page-container>
+      <RouterView class="q-py-lg" />
+    </q-page-container>
+  </q-layout>
+</template>
